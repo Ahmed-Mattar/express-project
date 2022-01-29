69c677d61e879248dee5f37e76d971c984af17c5
Mattar <acemattar@gmail.com>
2022-01-29 03:50:41 +0200

    feat: writing a middleware

--- server.js
+++ server.js
@@ -19,6 +19,18 @@ const friends = [
     }
 ];
 
+
+app.use((req, res, next) => {
+
+    const start = Date.now();
+
+    next();
+    // actions go here
+    const delta = Date.now() - start;
+    console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
+
 app.get('/friends', (req, res) => {
     res.json(friends);
 
@@ -28,7 +40,7 @@ app.get('/friends/:id', (req, res) => {
     const id = Number(req.params.id);
     const friend = friends[id];
     if (friend) {
-        res.json(friends[id]);
+        res.status(200).json(friends[id]);
     } else {
         res.status(404).json({
             error: "Friend does not exist"
