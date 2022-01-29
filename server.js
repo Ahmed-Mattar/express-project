6a09a628ffd89b167d8bef17d63146f60d1dbeb5
Mattar <acemattar@gmail.com>
2022-01-29 04:07:06 +0200

    feat: implement post request handler for adding a friend

--- server.js
+++ server.js
@@ -19,17 +19,40 @@ const friends = [
     }
 ];
 
-
+/*
+middleware to calculate the time for processing a request
+ */
 app.use((req, res, next) => {
-
     const start = Date.now();
-
     next();
     // actions go here
     const delta = Date.now() - start;
     console.log(`${req.method} ${req.url} ${delta}ms`);
 });
 
+
+app.use(express.json());
+
+
+app.post('/friends', (req, res) => {
+
+    if (!req.body.name) {
+        return res.status(400).json({
+            error: 'Missing friend name'
+        });
+    }
+
+    const newFriend = {
+        id: friends.length,
+        name: req.body.name
+    };
+
+    friends.push(newFriend);
+
+    res.json(newFriend);
+
+
+});
 
 app.get('/friends', (req, res) => {
     res.json(friends);
