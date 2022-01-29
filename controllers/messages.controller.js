939cbe21b6a7346f49fbc53a69fa2fcfb703525b
Mattar <acemattar@gmail.com>
2022-01-29 19:54:07 +0200

    feat: experimenting with template engines

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,7 +1,11 @@
 const path = require('path');
 
 function getMessages(req, res) {
-    res.sendFile(path.join(__dirname, '..', 'public', 'images', 'river.jpg'));
+    //res.sendFile(path.join(__dirname, '..', 'public', 'images', 'river.jpg'));
+    res.render('messages', {
+        title: "Messages to my friends!",
+        friend: "Mattar"
+    });
 };
 
 
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const messagesRouter = require('./routes/messages.router');
 
 const app = express();
 
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
 const PORT = 3000;
 
 /*
@@ -23,7 +26,12 @@ app.use((req, res, next) => {
 app.use('/site', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
-
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'My Friends Are Very Clever',
+        caption: 'Let\'s go swimming'
+    });
+});
 app.use('/friends', friendsRouter);
 app.use('/messages', messagesRouter);
 
